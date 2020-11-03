91d05fce5af4c86cf3d3665c345b46e6fcfef8fc
Anubhav-2 <73878920+Anubhav-2@users.noreply.github.com>
2020-11-03 14:35:21 +0530

    Create RHEL.sql

---
+++ Resources/Reports/RHEL.sql
@@ -0,0 +1,9 @@
+﻿Select Top 1000000 tblAssets.AssetID As [Asset ID],
+ tblAssets.AssetName,
+ tblAssets.Lastseen,
+ tblAssets.Lasttried
+ From tblAssets
+ Inner Join tblAssetCustom On tblAssets.AssetID = tblAssetCustom.AssetID
+ Inner Join tblAssetGroupLink On tblAssets.AssetID = tblAssetGroupLink.AssetID
+ Inner Join tblAssetGroups On tblAssetGroups.AssetGroupID = tblAssetGroupLink.AssetGroupID
+ Where tblAssetCustom.State = 1 And tblAssetGroups.AssetGroup Like 'IT Managed Redhat'
